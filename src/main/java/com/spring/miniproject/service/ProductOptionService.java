65d3f169f59d7b934b0e51105f6bec1fddf71164
KYK <kezy@daum.net>
2022-07-13 10:53:43 +0900

    [Feature] product-manage - product_option 제작중 #1

---
+++ src/main/java/com/spring/miniproject/service/ProductOptionService.java
@@ -0,0 +1,21 @@
+package com.spring.miniproject.service;
+
+import com.spring.miniproject.domain.ProductOptionDto;
+
+import java.util.List;
+
+public interface ProductOptionService {
+    int insertProductOption(ProductOptionDto productOptionDto) throws Exception;
+
+    ProductOptionDto selectProductOption(Integer option_id) throws Exception;
+
+    List<ProductOptionDto> selectAllProductOption() throws Exception;
+
+    int countProductOption() throws Exception;
+
+    int updateProductOption(ProductOptionDto productOptionDto) throws Exception;
+
+    int deleteProductOption(Integer option_id) throws Exception;
+
+    int deleteAllProductOption() throws Exception;
+}
---
+++ src/main/java/com/spring/miniproject/service/ProductOptionServiceImpl.java
@@ -0,0 +1,50 @@
+package com.spring.miniproject.service;
+
+import com.spring.miniproject.dao.ProductOptionDao;
+import com.spring.miniproject.domain.ProductOptionDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductOptionServiceImpl implements ProductOptionService {
+
+    @Autowired
+    ProductOptionDao productOptionDao;
+
+    @Override
+    public int insertProductOption(ProductOptionDto productOptionDto) throws Exception {
+        return productOptionDao.insertProductOption(productOptionDto);
+    }
+
+    @Override
+    public ProductOptionDto selectProductOption(Integer option_id) throws Exception {
+        return productOptionDao.selectProductOption(option_id);
+    }
+
+    @Override
+    public List<ProductOptionDto> selectAllProductOption() throws Exception {
+        return productOptionDao.selectAllProductOption();
+    }
+
+    @Override
+    public int countProductOption() throws Exception {
+        return productOptionDao.countProductOption();
+    }
+
+    @Override
+    public int updateProductOption(ProductOptionDto productOptionDto) throws Exception {
+        return productOptionDao.updateProductOption(productOptionDto);
+    }
+
+    @Override
+    public int deleteProductOption(Integer option_id) throws Exception {
+        return productOptionDao.deleteProductOption(option_id);
+    }
+
+    @Override
+    public int deleteAllProductOption() throws Exception {
+        return productOptionDao.deleteAllProductOption();
+    }
+}

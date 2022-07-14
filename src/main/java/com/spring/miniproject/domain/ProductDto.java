ba9c7921990aff1816980c5b9ad79b237848614a
KYK <kezy@daum.net>
2022-07-14 16:46:14 +0900

    [Feature] Product Mapper, Dao, Service 1차 완성 #1

--- src/main/java/com/spring/miniproject/domain/ProductDto.java
+++ src/main/java/com/spring/miniproject/domain/ProductDto.java
@@ -11,11 +11,13 @@ public class ProductDto {
     private Integer category_id;
     private Integer sub_category_id;
 
+    private String product_thumb_nail;
+
     // 기본 생성자
     public ProductDto() {}
 
     // 생성자
-    public ProductDto(Integer product_id, String product_name, String product_title, String product_info, Integer product_price, Integer category_id, Integer sub_category_id) {
+    public ProductDto(Integer product_id, String product_name, String product_title, String product_info, Integer product_price, Integer category_id, Integer sub_category_id, String product_thumb_nail) {
         this.product_id = product_id;
         this.product_name = product_name;
         this.product_title = product_title;
@@ -23,6 +25,7 @@ public class ProductDto {
         this.product_price = product_price;
         this.category_id = category_id;
         this.sub_category_id = sub_category_id;
+        this.product_thumb_nail = product_thumb_nail;
     }
 
     // getter, setter
@@ -82,21 +85,31 @@ public class ProductDto {
         this.sub_category_id = sub_category_id;
     }
 
+    public String getProduct_thumb_nail() {
+        return product_thumb_nail;
+    }
+
+    public void setProduct_thumb_nail(String product_thumb_nail) {
+        this.product_thumb_nail = product_thumb_nail;
+    }
+
     // equals, hashCode
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof ProductDto)) return false;
         ProductDto that = (ProductDto) o;
-        return Objects.equals(getProduct_id(), that.getProduct_id()) && Objects.equals(getProduct_name(), that.getProduct_name()) && Objects.equals(getProduct_title(), that.getProduct_title()) && Objects.equals(getProduct_info(), that.getProduct_info()) && Objects.equals(getProduct_price(), that.getProduct_price()) && Objects.equals(getCategory_id(), that.getCategory_id()) && Objects.equals(getSub_category_id(), that.getSub_category_id());
+        return Objects.equals(getProduct_id(), that.getProduct_id()) && Objects.equals(getProduct_name(), that.getProduct_name()) && Objects.equals(getProduct_title(), that.getProduct_title()) && Objects.equals(getProduct_info(), that.getProduct_info()) && Objects.equals(getProduct_price(), that.getProduct_price()) && Objects.equals(getCategory_id(), that.getCategory_id()) && Objects.equals(getSub_category_id(), that.getSub_category_id()) && Objects.equals(getProduct_thumb_nail(), that.getProduct_thumb_nail());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getProduct_id(), getProduct_name(), getProduct_title(), getProduct_info(), getProduct_price(), getCategory_id(), getSub_category_id());
+        return Objects.hash(getProduct_id(), getProduct_name(), getProduct_title(), getProduct_info(), getProduct_price(), getCategory_id(), getSub_category_id(), getProduct_thumb_nail());
     }
 
     // toString
+
+
     @Override
     public String toString() {
         return "ProductDto{" +
@@ -107,6 +120,7 @@ public class ProductDto {
                 ", product_price=" + product_price +
                 ", category_id=" + category_id +
                 ", sub_category_id=" + sub_category_id +
+                ", product_thumb_nail='" + product_thumb_nail + '\'' +
                 '}';
     }
 
---
+++ src/main/java/com/spring/miniproject/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.spring.miniproject.service;
+
+import com.spring.miniproject.domain.ProductDto;
+
+import java.util.List;
+
+public interface ProductService {
+    int insertProduct(ProductDto productDto) throws Exception;
+
+    ProductDto selectProduct(Integer product_id) throws Exception;
+
+    List<ProductDto> selectAllProduct() throws Exception;
+
+    int updateProduct(ProductDto productDto) throws Exception;
+
+    int deleteProduct(Integer product_id) throws Exception;
+
+    int deleteAllProduct() throws Exception;
+}

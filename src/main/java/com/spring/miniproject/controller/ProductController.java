aaaa6fe2485107bb85629eee1fc26b2342d1c73c
Renee <gracekang61@naver.com>
2022-07-14 17:12:55 +0900

    [Refactor]  이름, 내용변경

--- src/main/java/com/spring/miniproject/controller/ProductController.java
+++ src/main/java/com/spring/miniproject/controller/ProductController.java
@@ -1,7 +1,7 @@
 package com.spring.miniproject.controller;
 
 import com.spring.miniproject.domain.ProductReviewDto;
-import com.spring.miniproject.service.ProductDetailService;
+import com.spring.miniproject.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +10,9 @@ import java.util.List;
 
 @Controller
 @RequestMapping("/product")
-public class ProductDetailController {
+public class ProductController {
     @Autowired
-    private ProductDetailService productDetailService;
+    private ProductService productDetailService;
 
     @GetMapping("/detail")
     public String ProductDetailPage(){
@@ -20,31 +20,7 @@ public class ProductDetailController {
     }
 
 
-    /*
-    @GetMapping("/review")
-    @ResponseBody
-    public List<ProductReviewDto> ProductReview(String product_id){
-        System.out.println("컨트롤러!!!!!!!!!!!!!!!!!1");
-        System.out.println("product id = "+product_id);
-        List<ProductReviewDto> list = productDetailService.selectReview(Integer.parseInt(product_id));
-
-        for(ProductReviewDto p : list)
-            System.out.println(p.toString());
-        return list;
-    }*/
 
 
-    @GetMapping("/review")
-    @ResponseBody
-    public List<ProductReviewDto> ProductReview(@RequestParam String product_id){
-        System.out.println("컨트롤러!!!!!!!!!!!!!!!!!1");
-        System.out.println("product id = "+product_id);
-        List<ProductReviewDto> list = productDetailService.selectReview(Integer.parseInt(product_id));
-        System.out.println(list.get(0).toString());
-        for(ProductReviewDto p : list)
-            System.out.println(p.toString());
-
-        return list;
-    }
 
 }

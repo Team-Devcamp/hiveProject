b2f34676ef1c65b533bd4d4c5515098195de49ca
DevSeoRex <ch4570@naver.com>
2022-07-15 16:28:28 +0900

    [Feature] 유저 로그인 기능 & 회원가입 (유효성 검사 포함) 추가

--- src/main/java/com/spring/miniproject/dao/UserDaoImpl.java
+++ src/main/java/com/spring/miniproject/dao/UserDaoImpl.java
@@ -1,5 +1,6 @@
 package com.spring.miniproject.dao;
 
+import com.spring.miniproject.domain.UserDto;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,10 +11,25 @@ public class UserDaoImpl implements UserDao {
 	@Autowired
 	private SqlSession sqlSession;
 	private final String namespace = "com.spring.miniproject.dao.UserMapper.";
-	
+
 	@Override
-	public int count() {
-		return sqlSession.selectOne(namespace + "count");
+	public int insertUser(UserDto userDto) {
+		return sqlSession.insert(namespace+"insertUser", userDto);
+	}
+
+	@Override
+	public UserDto selectOneUser(String user_email) {
+		return sqlSession.selectOne(namespace+"selectUser",user_email);
+	}
+
+	@Override
+	public int updateUserPassword(UserDto userDto) {
+		return sqlSession.update(namespace+"updatePassword",userDto);
+	}
+
+	@Override
+	public int deleteOneUser(String user_email) {
+		return sqlSession.delete(namespace+"deleteOneUser",user_email);
 	}
 
 }
--- src/main/java/com/spring/miniproject/service/UserServiceImpl.java
+++ src/main/java/com/spring/miniproject/service/UserServiceImpl.java
@@ -1,17 +1,35 @@
 package com.spring.miniproject.service;
 
+import com.spring.miniproject.domain.UserDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.spring.miniproject.dao.UserDao;
 
 @Service
-public class UserServiceImpl implements UserService{
+public class UserServiceImpl implements UserService {
+
 	@Autowired
 	private UserDao userDao;
-	
+
 	@Override
-	public int count() {
-		return userDao.count();
+	public int insertUser(UserDto userDto) {
+		return userDao.insertUser(userDto);
+	}
+
+	@Override
+	public UserDto selectOneUser(String user_email) {
+		return userDao.selectOneUser(user_email);
+	}
+
+	@Override
+	public int updateUserPassword(UserDto userDto) {
+		return userDao.updateUserPassword(userDto);
+	}
+
+	@Override
+	public int deleteOneUser(String user_email) {
+		return userDao.deleteOneUser(user_email);
 	}
 }
+

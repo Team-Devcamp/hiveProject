2a8efeddbd3eec4b6979845e58ff99758dda39fb
Renee <gracekang61@naver.com>
2022-07-16 20:10:55 +0900

    [Feature]  qna게시판 목록, 글등록, 페이징처리 #2 세부요소 기능들 구현

--- src/main/java/com/spring/miniproject/controller/QnaController.java
+++ src/main/java/com/spring/miniproject/controller/QnaController.java
@@ -1,8 +1,11 @@
 package com.spring.miniproject.controller;
 
 import com.spring.miniproject.domain.QnaDto;
+import com.spring.miniproject.domain.QnaPaging;
 import com.spring.miniproject.service.QnaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,41 +23,68 @@ public class QnaController {
 
     @PostMapping("/write")
     @ResponseBody
-    public void insertQna(@RequestBody QnaDto dto, HttpSession session){
+    public ResponseEntity<String> insertQna(@RequestBody QnaDto qnaDto, @RequestParam int product_id, HttpSession session){
+//      String writer = (String)session.getAttribute("id");
 
-//        String writer = (String)session.getAttribute("id");
-        System.out.println("등록컨트롤??");
+        qnaDto.setProduct_id(product_id);
+        qnaDto.setWriter("asdf@gmail.com");
 
-        int product_id = 1;
-        dto.setWriter("asdf@gmail.com");
-        dto.setProduct_id(product_id);
+        try {
+            int rowCnt = qnaService.insertQna(qnaDto);
 
-        System.out.println("큐앤애이write product_id= " + product_id + " "+ dto.getQna_title());
-        qnaService.insertQna(dto);
+            if(rowCnt != 1) {
+                throw new Exception("글 등록에 실패했습니다.");
+            }
+
+            return new ResponseEntity<>("빠른 답변 드리겠습니다!", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("PRODUCTOPTION ADD ERR", HttpStatus.BAD_REQUEST);
+        }
+
     }
 
     @PostMapping("/delete")
-    public void deleteQna(@RequestParam int qna_id, int product_id, HttpSession session){
+    public ResponseEntity<String> deleteQna(@RequestParam int qna_id, int product_id, HttpSession session){
         System.out.println("product_id = " + qna_id+"  product_id = "+ product_id);
         String writer = "asdf@gmail.com";
 
-        qnaService.deleteQna(product_id, qna_id, writer);
+        try {
+            int rowCnt = qnaService.deleteQna(product_id, qna_id, writer);
+
+            if(rowCnt != 1){
+                throw new Exception("글 삭제에 실패했습니다.");
+            }
+            return new ResponseEntity<>("PRODUCTOPTION DELETE SUCCESS", HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("PRODUCTOPTION DELETE ERR", HttpStatus.BAD_REQUEST);
+        }
     }
 
     //상품별 qna목록
     @GetMapping(value="/list/product")
     @ResponseBody
-    public Map<String,Object> selectQnaList(@RequestParam int product_id){
-        List<QnaDto> list = qnaService.selectQnaList(product_id);
-        Map<String, Object> map = new HashMap<>();
-        map.put("list",list);
+    public Map<String,Object> selectQnaList(@RequestParam int product_id,
+                                            @RequestParam(required = false, defaultValue="1") String pg){
+        Map<String,Object> map = null;
+        try {
+            map = qnaService.selectQnaList(product_id, pg);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return map;
     }
 
     @GetMapping("/count")
     @ResponseBody
-    public int countQna(){
-        return qnaService.countQna();
+    public int countQna(@RequestParam Integer product_id) throws Exception{
+        System.out.println("카운트??"+qnaService.countQna(product_id));
+
+        return qnaService.countQna(product_id);
     }
+
 
 }

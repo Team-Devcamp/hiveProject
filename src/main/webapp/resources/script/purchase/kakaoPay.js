c06a06e0bfe0029d011b62ae7fab9b0281d5a907
Renee <gracekang61@naver.com>
2022-07-22 16:59:59 +0900

    [Feature] 결제기능 테스트중

--- src/main/webapp/resources/script/purchase/kakaoPay.js
+++ src/main/webapp/resources/script/purchase/kakaoPay.js
@@ -1,5 +1,27 @@
 $(function(){
     $('#purchaseBtn').on('click',function(){
+
+        if($('#user_name').val()==null){
+            alert("이름을 입력하세요");
+        }
+        if($('#b_tel').val()==null){
+            alert("주문자번호를 입력하세요");
+        }
+        if($('#user_email').val()==null){
+            alert("이메일을 입력하세요");
+        }
+
+        if($('#receiver_name').val()==null){
+            alert("수령인 성함을 입력하세요");
+        }
+        if($('#address_detail').val()==null){
+            alert("상세주소를 입력하세요");
+        }
+        if($('#receiver_phone').val()==null){
+            alert("수령인 전화번호를 입력하세요");
+        }
+
+
         if(!confirm("상품을 주문하시겠습니까?")){
             return;
         }
